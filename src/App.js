fd002d794176e953c52a6671a8158bfb35ee9079
shashikumar-bsk <shashikumar880188@gmail.com>
2025-03-19 19:22:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import ProductDetails from "./pages/ProductDetails";
+import ComparePage from "./pages/ComparePage";
+import "./styles/App.css";
 
-function App() {
+const App = () => {
+  const [compareList, setCompareList] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <div className="main-content">
+          <Sidebar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProductDetails
+                  compareList={compareList}
+                  setCompareList={setCompareList}
+                />
+              }
+            />
+            <Route
+              path="/compare"
+              element={
+                <ComparePage
+                  compareList={compareList}
+                  setCompareList={setCompareList}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
